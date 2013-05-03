3be87e27386713d1d9f0603c7e85bbf02dc828a7
hayajo <hayajo@cpan.org>
2013-05-04 08:55:18 +0900

    Refactored

--- lib/Mojolicious/Sessions/Storable.pm
+++ lib/Mojolicious/Sessions/Storable.pm
@@ -18,9 +18,10 @@ sub new {
     my $class = shift;
     my $self  = $class->SUPER::new(@_);
 
-    if ( $self->session_store ) {
-        Mojo::Base::attr(
-            'Mojolicious::Controller',
+    if ( $self->session_store
+        && !Mojolicious::Controller->can('session_options') )
+    {
+        Mojolicious::Controller->attr(
             session_options => sub {
                 $_[0]->stash->{'mojox.session.options'}
             },
@@ -35,21 +36,23 @@ sub load {
 
     return $self->SUPER::load($c) unless ( $self->session_store );
 
-    my ( $session_id, $session ) = $self->get_session($c);
+    my $stash = $c->stash;
 
+    my ( $session_id, $session ) = $self->get_session($c);
     unless ( $session_id && $session ) {
         $session_id = $self->generate_id( $c->req->env );
-        $session    = {};
     }
-
-    my $stash = $c->stash;
     $stash->{'mojox.session.options'} = { id => $session_id };
+    return unless $session;
+
+    # "expiration" value is inherited
+    my $expiration = $session->{expiration} // $self->default_expiration;
+    return if !(my $expires = delete $session->{expires}) && $expiration;
+    return if defined $expires && $expires <= time;
 
     return unless $stash->{'mojo.active_session'} = keys %$session;
-
     $stash->{'mojo.session'} = $session;
-    $session->{flash} = delete $session->{new_flash}
-        if $session->{new_flash};
+    $session->{flash} = delete $session->{new_flash} if $session->{new_flash};
 }
 
 sub store {
@@ -57,13 +60,10 @@ sub store {
 
     return $self->SUPER::store($c) unless ( $self->session_store );
 
+    # Make sure session was active
     my $stash = $c->stash;
-    my $session = $stash->{'mojo.session'} || {};
-
-    unless ( keys %$session || $stash->{'mojo.active_session'} ) {
-        $self->session_store->remove( $c->session_options->{id} );
-        return;
-    }
+    return unless my $session = $stash->{'mojo.session'};
+    return unless keys %$session || $stash->{'mojo.active_session'};
 
     # Don't reset flash for static files
     my $old = delete $session->{flash};
@@ -75,46 +75,9 @@ sub store {
     my $expiration = $session->{expiration} // $self->default_expiration;
     my $default = delete $session->{expires};
     $session->{expires} = $default || time + $expiration
-        if ( $expiration || $default );
+        if $expiration || $default;
 
-    $self->set_session( $c, $session );
-}
-
-sub generate_id {
-    my ( $self, $env ) = @_;
-    $self->sid_generator->($env);
-}
-
-sub get_session {
-    my ( $self, $c ) = @_;
-
-    my $session_id = $c->signed_cookie( $self->cookie_name ) or return;
-    my $session    = $self->session_store->fetch($session_id) or return;
-
-    my $expires = delete $session->{expires};
-    if ( defined $expires && $expires <= time ) {
-        $self->session_store->remove($session_id);
-        return;
-    };
-
-    return ( $session_id, $session );
-}
-
-sub set_session {
-    my ( $self, $c, $session ) = @_;
-
-    if ( $c->session_options->{expire} || $session->{expires} <= time ) {
-        $session->{expires} = 1;
-        $self->session_store->remove( $c->session_options->{id} );
-    }
-    elsif ( $c->session_options->{change_id} ) {
-        $self->session_store->remove( $c->session_options->{id} );
-        $c->session_options->{id} = $self->generate_id( $c->req->env );
-        $self->session_store->store( $c->session_options->{id}, $session );
-    }
-    else {
-        $self->session_store->store( $c->session_options->{id}, $session );
-    }
+    $self->set_session( $c, $session ) unless $c->session_options->{no_store};
 
     my $options = {
         domain   => $self->cookie_domain,
@@ -128,6 +91,37 @@ sub set_session {
         $c->session_options->{id},
         $options,
     );
+}
+
+sub generate_id {
+    my ( $self, $env ) = @_;
+    $self->sid_generator->($env);
+}
+
+sub get_session {
+    my ( $self, $c ) = @_;
+
+    my $session_id = $c->signed_cookie( $self->cookie_name ) or return;
+    my $session    = $self->session_store->fetch($session_id) or return;
+
+    return ( $session_id, $session );
+}
+
+sub set_session {
+    my ( $self, $c, $session ) = @_;
+
+    if ( $c->session_options->{expire} || ( defined $session->{expires} && $session->{expires} <= time ) ) {
+        $session->{expires} = 1;
+        $self->session_store->remove( $c->session_options->{id} );
+    }
+    elsif ( $c->session_options->{change_id} ) {
+        $self->session_store->remove( $c->session_options->{id} );
+        $c->session_options->{id} = $self->generate_id( $c->req->env );
+        $self->session_store->store( $c->session_options->{id}, $session );
+    }
+    else {
+        $self->session_store->store( $c->session_options->{id}, $session );
+    }
 }
 
 1;
