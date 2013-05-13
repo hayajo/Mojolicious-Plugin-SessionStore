cf889a6a6636ee22794ce17de7548f75244ed061
hayajo <hayajo@cpan.org>
2013-05-14 08:36:53 +0900

    0.03

--- lib/Mojolicious/Plugin/SessionStore.pm
+++ lib/Mojolicious/Plugin/SessionStore.pm
@@ -1,7 +1,7 @@
 package Mojolicious::Plugin::SessionStore;
 use Mojo::Base 'Mojolicious::Plugin';
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 use Mojolicious::Sessions::Storable;
 
--- lib/Mojolicious/Sessions/Storable.pm
+++ lib/Mojolicious/Sessions/Storable.pm
@@ -3,7 +3,7 @@ package Mojolicious::Sessions::Storable;
 use strict;
 use warnings;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 use Mojo::Base 'Mojolicious::Sessions';
 use Digest::SHA1 ();
