aa383825f5ae288cb8c19b699e1ee5fb7b500a72
hayajo <hayajo@cpan.org>
2013-05-17 11:55:37 +0900

    0.04

--- lib/Mojolicious/Sessions/Storable.pm
+++ lib/Mojolicious/Sessions/Storable.pm
@@ -3,7 +3,7 @@ package Mojolicious::Sessions::Storable;
 use strict;
 use warnings;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 use Mojo::Base 'Mojolicious::Sessions';
 use Digest::SHA1 ();
